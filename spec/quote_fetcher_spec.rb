f8ad0e27d3465a55fa2e68b3f159607b04b23253
Sabron13 <kurt.schnelker@gmail.com>
2015-06-12 11:43:31 -0400

    sets up TestApp for testing

--- spec/quote_fetcher_spec.rb
+++ spec/quote_fetcher_spec.rb
@@ -1,4 +1,12 @@
-require "../quote_fetcher.rb"
+require_relative "../quote_fetcher"
+require 'rack'
+
+class TestApp
+  def call(env)
+    @env = env
+    [200, {"Content-Type" => "text/html"}, ["OK"]]
+  end
+end
 
 describe QuoteFetcher do
   let (:app) { lambda {[200, {"Content-Type" => "text/plain"}, ['this is a dummy app']]} }
