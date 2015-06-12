7b1445de20355bba9c8de3b00d49e1fad8e20b70
Sabron13 <kurt.schnelker@gmail.com>
2015-06-12 14:04:17 -0400

    isolated bug to testapp's response

--- spec/quote_fetcher_spec.rb
+++ spec/quote_fetcher_spec.rb
@@ -11,14 +11,27 @@ end
 describe QuoteFetcher do
   let(:testapp) { TestApp.new }
   let(:middleware) { QuoteFetcher.new(testapp) }
+  let(:quotes) { middleware.quotes }
 
-  context "when called with a get request" do
+  context "when called with a GET request to the correct route" do
     let(:request) { Rack::MockRequest.new(testapp) }
-    let(:response) { request.get('/quote')}
+    let(:response) { request.get('/quote') }
 
     it "responds with a quote" do
-      expect(middleware.quotes.include? response.body)
+      puts quotes
+      puts response.body
+      expect(quotes.include? response.body).to eq(true)
     end
-
   end
+
+  context "when called with a get request to the wrong route" do
+    let(:request) { Rack::MockRequest.new(testapp) }
+    let(:response) { request.get('/wrongroute') }
+
+    it "doesn't return a quote" do
+      expect(quotes.include? response.body).to eq(false)
+    end
+  end
+
+
 end
